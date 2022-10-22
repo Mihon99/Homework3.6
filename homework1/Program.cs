d5f4021aaa2ab515f6cbd4ffcb1430d5d989b520
reaper <reaper>
2022-10-22 17:12:23 +0300

    homework1

---
+++ homework1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите массив чисел: ");
+string getArray = Console.ReadLine();
+string[] array = getArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] inputArray = new int[array.Length];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    inputArray[i] = int.Parse(array[i]);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (inputArray[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine("Чисел больше нуля в массиве = " + count);
